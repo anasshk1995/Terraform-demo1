440b39c32686a7086bb64f69b54a4d399ba03f23
anasshk1995 <140480958+anasshk1995@users.noreply.github.com>
2024-07-10 11:26:18 +0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,18 +1,6 @@
-
-variable "domain" {
-  type = string
-  default = "craigdunn.net"
-}
-
-
 variable "instance_name" {
   type = string
-  default = "terraform-demo"
-}
-
-variable "ami_id" {
-  type = string
-  default = "ami-06d94a781b544c133"
+  default = "terraform-demo1"
 }
 
 variable "instance_type" {
@@ -22,17 +10,12 @@ variable "instance_type" {
 
 variable "subnet" {
   type = string
-  default = "subnet-01a4fdab38dabcf25"
-}
-
-variable "aws_keypair" {
-  type = string
-  default = "bolt"
+  default = "subnet-04d6377ceb81f90d6"
 }
 
 variable "region" {
   type = string
-  default = "eu-west-1"
+  default = "us-east-1"
 }
 
 
