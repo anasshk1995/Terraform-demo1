69303fc5414e251b84ade210e445a34474ce31eb
anasshk1995 <140480958+anasshk1995@users.noreply.github.com>
2024-07-10 12:16:17 +0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,17 @@
+variable "domain" {
+  type = string
+  default = ""
+}
+
+
 variable "instance_name" {
   type = string
-  default = "terraform-demo1"
+  default = "terraform-demo"
+}
+
+variable "ami_id" {
+  type = string
+  default = ""
 }
 
 variable "instance_type" {
@@ -10,12 +21,15 @@ variable "instance_type" {
 
 variable "subnet" {
   type = string
-  default = "subnet-04d6377ceb81f90d6"
+  default = "subnet-01a4fdab38dabcf25"
+}
+
+variable "aws_keypair" {
+  type = string
+  default = "bolt"
 }
 
 variable "region" {
   type = string
   default = "us-east-1"
 }
-
-
